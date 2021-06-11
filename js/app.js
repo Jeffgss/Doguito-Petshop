2bd1550a2be18b1b83a6b36f4f7aa7be044f22b4
Jeff Santos <jefferson.santos@maxmilhas.com.br>
2021-06-11 12:32:34 -0300

    03-add object with error messages and validate erros to show on screen

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import { valida } from "./validacao.js";
 
 const inputs = document.querySelectorAll("input");
 
-input.forEach((input) => {
+inputs.forEach((input) => {
   input.addEventListener("blur", (evento) => {
     valida(evento.target);
   });
